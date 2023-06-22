ebe75d19d3fa99ccbfed3b9ba2898a95b8b59d1f
Neetika Tandon <69705083+Neetika146@users.noreply.github.com>
2023-06-22 17:38:00 +0530

    kth

---
+++ kth_smallestnodeBST.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h> 
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+int cnt = 0;
+int kthSmallest(TreeNode<int> *root, int &k)
+{
+        if (!root) return -1;        
+        int left = kthSmallest(root->left, k);        
+        if(left != -1) return left;
+        k--;
+        if(k == 0) return root->data;
+        int right =  kthSmallest(root->right, k);
+        if(right != -1) return right;
+        return -1;
+	//	Write the code here.
+}
