e943c78c6f802f525d2a3cec1b992ad35a6de56d
Neetika Tandon <69705083+Neetika146@users.noreply.github.com>
2023-06-23 15:12:25 +0530

    medianFinder

---
+++ medianFinder.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+
+class MedianFinder {
+public:
+    priority_queue<int, vector<int>> maxHeap;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        if(maxHeap.size() == 0 || maxHeap.top() >= num){
+            maxHeap.push(num);
+        }
+        else minHeap.push(num);
+
+        if(maxHeap.size() < minHeap.size()){
+            int ele = minHeap.top();
+            minHeap.pop();
+            maxHeap.push(ele);
+        }
+        else if(maxHeap.size() > minHeap.size() + 1){
+            int ele = maxHeap.top();
+            maxHeap.pop();
+            minHeap.push(ele);
+        }
+    }
+    
+    int findMedian() {
+        if(maxHeap.size() > minHeap.size()){
+            return maxHeap.top();
+        }
+        return maxHeap.empty() ? 0 : floor(maxHeap.top()+ minHeap.top())/2;
+    }
+};
+
+
+vector<int> findMedian(vector<int> &arr, int n){
+	vector<int> ans;
+	MedianFinder m;
+	for(int i = 0; i < n; i++){
+		m.addNum(arr[i]);
+		ans.push_back(m.findMedian());
+	}
+
+	return ans;
+	// Write your code here 
+}
