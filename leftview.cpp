7300f986792565a9f0c93a3143be499c5373d74d
Neetika Tandon <69705083+Neetika146@users.noreply.github.com>
2023-08-07 22:37:23 +0530

    dx

--- leftview.cpp
+++ leftview.cpp
@@ -35,3 +35,23 @@ vector<int> getLeftView(TreeNode<int> *root)
     return ans;
    // Your code here
 }
+
+
+// vector<int> leftView(Node *root)
+// {
+//    vector<int> ans;
+//    if(!root) return ans;
+//    queue<Node*> q;
+//    q.push(root);
+//    while(!q.empty()){
+//        int s=q.size();
+//        ans.push_back(q.front()->data);
+//        while(s--){
+//            Node* t=q.front();
+//            q.pop();
+//            if(t->left) q.push(t->left);
+//            if(t->right) q.push(t->right);
+//        }
+//    }
+//    return ans;
+// }
